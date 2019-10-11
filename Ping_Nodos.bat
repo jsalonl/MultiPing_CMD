ec3e78597a2dcf984271d545f3206f628aaea58b
Salomon Nieto <joansalomon@gmail.com>
2019-10-11 15:41:28 -0500

    Corregido nombre de fichero .txt dentro del script

--- Ping_Nodos.bat
+++ Ping_Nodos.bat
@@ -1,4 +1,5 @@
 @echo off
 echo Ejecutando pruebas
-for /f UseBackQ %%a in (ips.txt) do ping -n 2 %%a|Find "TTL=">Nul && Echo Host [%%a] Online ||Echo Host [%%a] offline
+for /f UseBackQ %%a in (hosts.txt) do ping -n 2 %%a|Find "TTL=">Nul && Echo Host [%%a] Online ||Echo Host [%%a] offline
+echo Pruebas terminadas
 pause
